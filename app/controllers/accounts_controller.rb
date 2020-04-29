3b70e5d8605c2353547016c0bdb2403e159fe37b
NotNelson <codepolitics@gmail.com>
2020-04-29 00:25:09 -0300

    Finished accounts controller and forms

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -21,13 +21,27 @@ class AccountsController < ApplicationController
     end
   end
 
+  def edit
+    @account = Account.find(params[:id])
+  end
+
   def update
+    @account = Account.find(params[:id])
+
+    if @account.update(account_params)
+      @account.encrypt_password
+      if @account.save
+        redirect_to @account
+      end
+    end
   end
 
   def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+
+    redirect_to accounts_path
   end
-
-
 
   private
   def account_params
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,9 +1,19 @@
 module AccountsHelper
+  # Return a Json object with additional account data
   def get_data(account)
-    json = []
+    account.get_usage
+  end
 
-    json << account.get_usage
-
-    json
+  # Take the usage part of the Json data and turn it into a format that can be
+  # used to generate a chart. TODO: this is quite esoteric, refactor one day.
+  def parse_table_data(json)
+    data = []
+    json["daily"].each do |item|
+      item = item.to_s.gsub('{', '').gsub('}', '')
+      date = item[0..item.index('>')-2]
+      usage = item[item.index('>')+1..item.length].gsub('"', '')
+      data << [date, usage]
+    end
+    data
   end
 end
